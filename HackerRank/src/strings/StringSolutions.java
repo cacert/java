8efb903e1c5c58468029c0c6fb1843b1acb8166d
Kasim Sert <kasimsert@gmail.com>
2017-05-08 00:27:00 +0300

    ok

--- HackerRank/src/strings/StringSolutions.java
+++ HackerRank/src/strings/StringSolutions.java
@@ -1,7 +1,10 @@
 package strings;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.hackerrank.linear.Stack;
 
 public class StringSolutions {
 	public int countCode(String str) {
@@ -23,10 +26,11 @@ public class StringSolutions {
 		return count;
 	}
 
-	public static void main(String[] args) {
-		System.err.println(new StringSolutions().countCode("cozfxxcope"));
-		System.err.println(new StringSolutions().compress("ccozfxxcope"));
-		System.err.println(new StringSolutions().checkIfUniqueChars("cozfxpe"));
+	public static void main(String[] args) throws Exception {
+//		System.err.println(new StringSolutions().countCode("cozfxxcope"));
+//		System.err.println(new StringSolutions().compress("ccozfxxcope"));
+//		System.err.println(new StringSolutions().checkIfUniqueChars("cozfxpe"));
+		StringSolutions.findPalindrome();
 	}
 	
 	public String compress(String input){
@@ -50,4 +54,74 @@ public class StringSolutions {
 			return true;
 		return input.length() == input.chars().distinct().count();
 	}
+	
+    public static void findPalindrome() throws Exception {
+        
+        Scanner scanner = new Scanner(new FileInputStream("input14.txt"));
+        int lineNum = Integer.parseInt(scanner.nextLine());
+        while(lineNum > 0){
+            String line = scanner.nextLine();
+            findPalindrome(line);
+            lineNum --;
+        }
+        scanner.close();
+    }
+    
+    private static void findPalindrome(String line ){
+        int lenght = line.length();
+        int j = 0;
+        boolean notFound = false;
+        int counter = -1;
+        for(int i =0 ; i< lenght/2 ;i++,j++){
+            if(line.charAt(i) == line.charAt(lenght-1-j))
+                continue;
+            else{
+                if(notFound){
+                    System.out.println(-1);
+                    return;
+                }
+                if(line.charAt(i+1)==line.charAt(lenght-1-j)){
+                	counter = i;
+                    i++;
+                    notFound = true;
+                }
+                else if(line.charAt(i)==line.charAt(lenght-1-j-1)){
+                	counter = lenght-1-j;
+                    j++;
+                    notFound =true;
+                }
+//                else {//break
+//                    System.out.println(counter);
+//                    return;
+//                }
+            }
+        }
+        System.out.println(counter);
+    }
+    private static void findPalindromeWithStack(String line ){
+    	Stack<Character> stack = new Stack<>();
+    	for(int i=0;i<line.length();i++){
+    		stack.push(line.charAt(i));
+    	}
+    	boolean notFound = false;
+    	int counter = -1;
+    	for(int i=0;i<line.length()/2;i++){
+    		char c = stack.pop();
+    		if(c != line.charAt(i)){
+    			if(notFound){
+    				System.out.println(-1);
+                    return;
+    			}
+    			if(stack.peek() == line.charAt(i)){
+    				stack.pop();
+    				continue;
+    			}
+    			else if(line.charAt(i+1) == c){
+    				i++;
+    			}
+    		}
+    	}
+    	System.out.println(counter);
+    }
+    
 }
